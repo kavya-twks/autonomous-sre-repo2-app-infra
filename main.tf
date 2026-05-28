6bc6fc2a913a572d7be246a539e9e1a60a7669e6
Kavya.T <kavya.t@thoughtworks.com>
2026-05-28 11:52:10 +0530

    updated code

--- main.tf
+++ main.tf
@@ -4,9 +4,11 @@ terraform {
     aws = { source = "hashicorp/aws", version = "~> 5.0" }
   }
   backend "s3" {
-    bucket = "autonomous-sre-tfstate"
-    key    = "app-infra/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "autonomous-sre-tfstate"
+    key            = "app-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "autonomous-sre-tflock"
+    encrypt        = true
   }
 }
 
